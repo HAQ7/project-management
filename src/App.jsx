070a9522a3a84711f18bc5038e4fac00fd4fb406
HAQ7 <hussam.a.qannam@gmail.com>
2024-03-04 12:29:31 +0300

    added localstorage func

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import { useRef, useState, useContext } from "react";
-import SideMenu from "./components/SideMenu.jsx";
-import ActiveProject from "./components/ActiveProject.jsx";
-import CreateProjectForm from "./components/CreateProjectForm.jsx";
-import Button from "./components/UI/Button.jsx";
-import {ThemeContext} from "./components/store/ThemeContext.jsx";
-
+import { useRef, useState, useContext } from 'react';
+import SideMenu from './components/SideMenu.jsx';
+import ActiveProject from './components/ActiveProject.jsx';
+import CreateProjectForm from './components/CreateProjectForm.jsx';
+import Button from './components/UI/Button.jsx';
+import { ThemeContext } from './components/store/ThemeContext.jsx';
 
 function App() {
-  const [projects, setProjects] = useState([]);
+  const [projects, setProjects] = useState(
+    JSON.parse(localStorage.getItem('projects')) === null
+      ? []
+      : JSON.parse(localStorage.getItem('projects'))
+  );
   const [activeProject, setActiveProject] = useState(0);
   const [hasGoneToCreate, setHasGoneToCreate] = useState(false);
-  const [theme, setTheme] = useState('light')
+  const [theme, setTheme] = useState(
+    localStorage.getItem('theme') === null
+      ? 'light'
+      : localStorage.getItem('theme')
+  );
 
   const projectCreateHandler = (project) => {
     setProjects((prevProjects) => {
       setActiveProject(projects.length);
-      return [...prevProjects, project];
+      const newProjects = [...prevProjects, project];
+      localStorage.setItem('projects', JSON.stringify(newProjects));
+      return newProjects;
     });
   };
 
@@ -25,6 +34,7 @@ function App() {
       if (activeProject === projects.length && activeProject !== 0) {
         setActiveProject(projects.length - 1);
       }
+      localStorage.setItem('projects', JSON.stringify(prevProjects));
       return [...prevProjects];
     });
   };
@@ -39,54 +49,74 @@ function App() {
 
   const addTaskHandler = (task) => {
     projects[activeProject].savedTasks.push(task);
-    setProjects((prevProjects) => [...prevProjects]);
+    setProjects((prevProjects) => {
+      localStorage.setItem('projects', JSON.stringify(prevProjects));
+      return [...prevProjects];
+    });
   };
 
   const removeTaskHandler = (taskIndex) => {
     projects[activeProject].savedTasks.splice(taskIndex, 1);
-    setProjects((prevProjects) => [...prevProjects]);
+    setProjects((prevProjects) => {
+      localStorage.setItem('projects', JSON.stringify(prevProjects));
+      return [...prevProjects];
+    });
   };
 
   return (
     <ThemeContext.Provider value={theme}>
-      <section className={`${theme == 'light' ? "bg-[#dddddd]" : "bg-[#181F25]" }  w-screen h-screen transition`}>
-      <SideMenu
-        projects={projects}
-        onProjectClick={changeActiveProject}
-        onProjectCreateClick={changeActiveProject}
-        onChangeTheme={() => {setTheme(() =>
-          theme == 'light' ? 'dark' : 'light'
-        )}}
-
-      />
-      <section className="flex flex-col justify-center items-center gap-3">
-        {activeProject === -1 ? (
-          <CreateProjectForm onProjectCreate={projectCreateHandler} hasLeftProject={hasGoneToCreate} />
-        ) : projects.length > 0 ? (
-          <ActiveProject
-            onProjectRemove={projectRemoveHandler}
-            onAddTask={addTaskHandler}
-            onRemoveTask={removeTaskHandler}
-            project={projects[activeProject]}
-            hasLeftProject={hasGoneToCreate}
-          />
-        ) : (
-          //   #202731
-          <h1
-            className={`font-bold mt-32 text-center ${theme == 'light' ? "bg-white text-black" : "bg-[#202731] text-white" }  shadow rounded-3xl p-2 grid place-items-center animate-bottomMoveUp duration-[0.25s] transition-all ${
-              hasGoneToCreate ? "translate-y-[100vh]" : ""
-            }`}
-          >
-            You have not added any projects yet...{" "}
-            <Button
-              onClick={() => changeActiveProject(-1)}
-              className="p-1 mt-3"
+      <section
+        className={`${
+          theme == 'light' ? 'bg-[#dddddd]' : 'bg-[#181F25]'
+        }  w-screen h-screen transition`}
+      >
+        <SideMenu
+          projects={projects}
+          onProjectClick={changeActiveProject}
+          onProjectCreateClick={changeActiveProject}
+          onChangeTheme={() => {
+            setTheme(() => {
+              const newTheme = theme == 'light' ? 'dark' : 'light';
+              localStorage.setItem('theme', newTheme);
+              return newTheme;
+            });
+          }}
+        />
+        <section className="flex flex-col justify-center items-center gap-3">
+          {activeProject === -1 ? (
+            <CreateProjectForm
+              onProjectCreate={projectCreateHandler}
+              hasLeftProject={hasGoneToCreate}
+            />
+          ) : projects.length > 0 ? (
+            <ActiveProject
+              onProjectRemove={projectRemoveHandler}
+              onAddTask={addTaskHandler}
+              onRemoveTask={removeTaskHandler}
+              project={projects[activeProject]}
+              hasLeftProject={hasGoneToCreate}
+            />
+          ) : (
+            //   #202731
+            <h1
+              className={`font-bold mt-32 text-center ${
+                theme == 'light'
+                  ? 'bg-white text-black'
+                  : 'bg-[#202731] text-white'
+              }  shadow rounded-3xl p-2 grid place-items-center animate-bottomMoveUp duration-[0.25s] transition-all ${
+                hasGoneToCreate ? 'translate-y-[100vh]' : ''
+              }`}
             >
-              <p>Create Project</p>
-            </Button>
-          </h1>
-        )}
-      </section>
+              You have not added any projects yet...{' '}
+              <Button
+                onClick={() => changeActiveProject(-1)}
+                className="p-1 mt-3"
+              >
+                <p>Create Project</p>
+              </Button>
+            </h1>
+          )}
+        </section>
       </section>
     </ThemeContext.Provider>
   );
--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,16 +1,16 @@
-import {useContext, useRef, useState} from "react";
-import Light from "./UI/Light.jsx";
-import {ThemeContext} from "./store/ThemeContext.jsx";
+import { useContext, useRef, useState } from 'react';
+import Light from './UI/Light.jsx';
+import { ThemeContext } from './store/ThemeContext.jsx';
 
 export default function SideMenu({
   projects,
   onProjectClick,
   onProjectCreateClick,
-    onChangeTheme
+  onChangeTheme,
 }) {
   const [sideBarOpen, setSideBarOpen] = useState(false);
   let hasCheckWindowInStart = useRef(false);
-  const listHeight =  3.75 * projects.length;
+  const listHeight = 3.75 * projects.length;
   const checkWindowSize = () => {
     if (window.innerWidth > 1280 && !sideBarOpen) {
       setSideBarOpen(true);
@@ -18,7 +18,7 @@ export default function SideMenu({
     }
     if (window.innerWidth < 1280 && sideBarOpen) setSideBarOpen(false);
   };
-  window.addEventListener("resize", checkWindowSize);
+  window.addEventListener('resize', checkWindowSize);
   if (!hasCheckWindowInStart.current) {
     hasCheckWindowInStart.current = true;
     checkWindowSize();
@@ -34,12 +34,12 @@ export default function SideMenu({
     <>
       <div
         className={`w-screen h-screen opacity-40 bg-black absolute transition-all z-10 xl:hidden block ${
-          sideBarOpen ? "visible" : "hidden"
+          sideBarOpen ? 'visible' : 'hidden'
         }`}
       />
       <img
         className={`w-[60px] cursor-pointer absolute top-0 left-0 transition-all z-30 xl:hidden block ${
-          !sideBarOpen ? "opacity-0" : ""
+          !sideBarOpen ? 'opacity-0' : ''
         }`}
         src="./menu-x.svg"
         alt=""
@@ -47,25 +47,29 @@ export default function SideMenu({
       />
       <img
         className={`w-[60px] cursor-pointer absolute top-0 left-0 transition-all z-30 xl:hidden block ${
-          sideBarOpen ? "opacity-0" : ""
+          sideBarOpen ? 'opacity-0' : ''
         }`}
-        src={`./${useContext(ThemeContext) == 'light' ? "menu-bars-black.svg" : "menu-bars-white.svg" }`}
+        src={`./${
+          useContext(ThemeContext) == 'light'
+            ? 'menu-bars-black.svg'
+            : 'menu-bars-white.svg'
+        }`}
         alt=""
         onClick={changeSideBarState}
       />
       {/*/!*#19454B #00B9D2 ${useContext(ThemeContext) == 'light' ? "bg-gray-200" : "bg-[#181F25] "} */}
       <section
-        className={`bg-gradient-to-b ${useContext(ThemeContext) == 'light' ? "from-[#19454B] to-[#00B9D2]" : "from-[#202731] to-[#181F25] " } w-72 ps-5 h-screen z-20 absolute top-0 rounded-e-3xl text-white duration-300 transition-all overflow-y-auto overflow-x-hidden ${
-          sideBarOpen ? "" : "translate-x-[-100%]"
+        className={`bg-gradient-to-b ${
+          useContext(ThemeContext) == 'light'
+            ? 'from-[#19454B] to-[#00B9D2]'
+            : 'from-[#202731] to-[#181F25] '
+        } w-72 ps-5 h-screen z-20 absolute top-0 rounded-e-3xl text-white duration-300 transition-all overflow-y-auto overflow-x-hidden ${
+          sideBarOpen ? '' : 'translate-x-[-100%]'
         }`}
       >
         <h1 className="font-bold text-3xl mt-[60px]">
-          Projects{" "}
-          <img
-            className={`w-[20px] inline`}
-            src="./project-white.svg"
-            alt=""
-          />
+          Projects{' '}
+          <img className={`w-[20px] inline`} src="./project-white.svg" alt="" />
         </h1>
         <ul
           className={`mt-3 transition-all duration-200`}
@@ -91,14 +95,12 @@ export default function SideMenu({
             changeSideBarState();
             onProjectCreateClick(-1);
           }}
-          className={
-            `bg-[#38A7B6] w-36 h-12 rounded-3xl shadow-xl hover:scale-105 transition-all duration-300 cursor-pointer font-medium mt-3`
-          }
+          className={`bg-[#38A7B6] w-36 h-12 rounded-3xl shadow-xl hover:scale-105 transition-all duration-300 cursor-pointer font-medium mt-3`}
         >
           Create Project
         </button>
       </section>
-        <Light onLightClick={onChangeTheme} isSideBarOpen={sideBarOpen}/>
+      <Light onLightClick={onChangeTheme} isSideBarOpen={sideBarOpen} />
     </>
   );
 }
