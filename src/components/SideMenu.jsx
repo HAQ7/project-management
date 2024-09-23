1d9a4051f8cf47630a98757928bd1787653582c3
Hussam Qannam <hussam.a.qannam@gmail.com>
2024-09-24 01:08:16 +0300

    asdasd

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -41,7 +41,7 @@ export default function SideMenu({
         className={`w-[60px] cursor-pointer absolute top-0 left-0 transition-all z-30 xl:hidden block ${
           !sideBarOpen ? 'opacity-0' : ''
         }`}
-        src="./menu-x.svg"
+        src="./menu-x-black.svg"
         alt=""
         onClick={changeSideBarState}
       />
